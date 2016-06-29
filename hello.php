3a29e5a09b9ed7e46cc8722ad19169af62b9b97c
krishnachirumamilla <mchirumamilla@mchirumami-ltm.internal.salesforce.com>
2016-06-29 11:57:24 -0700

    Hello page
    This page asks for user name!

---
+++ hello.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "<h1>Well, this is the hello page! </h1>";
+echo "<p>What is your name?</p>";
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello</title>
+</head>
+<body>
+<form action="index.php">
+	<input type="text" name="user_name">
+	<input type="button" value="submit" name="submit">
+</form>
+</body>
+</html>
